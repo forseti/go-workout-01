f97f615141dae7434730f5b6e4cbc90b9b4feaa9
forseti <slickwolf@gmail.com>
2019-10-15 00:48:28 +0800

    Add type Bitcoin and implement Stringer

--- ch06/banking/wallet.go
+++ ch06/banking/wallet.go
@@ -2,15 +2,21 @@ package banking
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w* Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w* Wallet) Deposit(amount Bitcoin) {
+	// fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w* Wallet) Balance() int {
+func (w* Wallet) Balance() Bitcoin {
 	return w.balance
 }
