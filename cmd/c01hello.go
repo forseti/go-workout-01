afe2516dddae79854c4a71f508b581b3085a3426
forseti <slickwolf@gmail.com>
2019-10-20 23:36:50 +0800

    Add cmd package to test impl in main

---
+++ cmd/c01hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"forseti.github.io/goworkout/c01hello"
+)
+
+func main() {
+	fmt.Println(c01hello.Hello("world", c01hello.Spanish))
+}
---
+++ cmd/c08di_console.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"forseti.github.io/goworkout/c08di"
+	"os"
+)
+
+func main()  {
+	c08di.Greet(os.Stdout, "Elodie")
+}
